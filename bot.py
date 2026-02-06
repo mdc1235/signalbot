138ef1487415ecc690977471f7f6252fe4f2ab2f
mdc1235 <mdc123521@gmail.com>
2026-02-06 09:37:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,20 +3,17 @@ import os
 import telebot
 import random
 import time
-
 from datetime import datetime, timedelta
+import threading
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = "6181352243"
-
-if not BOT_TOKEN:
-    raise Exception("BOT_TOKEN not found in environment variables")
 
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode="HTML")
 
 pairs = ["EUR/USD", "GBP/USD", "USD/JPY", "AUD/USD", "USD/CHF"]
 markets = ["REAL MARKET", "OTC"]
-platforms = ["Pocket Option"
+platforms = ["Pocket Option"]
 directions = ["UP 📈", "DOWN 📉"]
 
 @bot.message_handler(commands=["start"])
@@ -24,9 +21,9 @@ def start(msg):
     bot.reply_to(
         msg,
         "🔥 <b>VIP SIGNAL BOT STARTED</b>\n\n"
-        "⏰ Entry time ke sath signals aayenge\n"
-        "📊 OTC / REAL MARKET\n"
-        "💹 Pocket Option 
+        "⏱ Entry time ke sath signals aayenge\n"
+        "📊 REAL / OTC MARKET\n"
+        "💹 Pocket Option"
     )
 
 def send_signal():
@@ -35,26 +32,27 @@ def send_signal():
     market = random.choice(markets)
     platform = random.choice(platforms)
 
+    # time system
     signal_time = datetime.now()
-entry_time = signal_time + timedelta(minutes=1)
+    entry_time = signal_time + timedelta(minutes=1)
 
     text = (
-f"📊 <b>{platform}</b>\n"
-f"🟡 <b>{market}</b>\n\n"
-f"💱 Pair: <b>{pair}</b>\n"
-f"📈 Direction: <b>{direction}</b>\n\n"
-f"🕒 Signal Time: <b>{signal_time.strftime('%H:%M:%S')}</b>\n"
-f"⏰ Entry Time: <b>{entry_time.strftime('%H:%M:%S')}</b>\n"
-f"⌛ Expiry: 1 Minute"
-)
+        f"📊 <b>{platform}</b>\n"
+        f"🟡 <b>{market}</b>\n\n"
+        f"💱 Pair: <b>{pair}</b>\n"
+        f"📈 Direction: <b>{direction}</b>\n\n"
+        f"🕒 Signal Time: <b>{signal_time.strftime('%H:%M:%S')}</b>\n"
+        f"⏰ Entry Time: <b>{entry_time.strftime('%H:%M:%S')}</b>\n"
+        f"⌛ Expiry: 1 Minute"
+    )
 
     bot.send_message(CHAT_ID, text)
 
 def loop():
     while True:
         send_signal()
-        time.sleep(60) 
-import threading
+        time.sleep(60)  # 1 min gap
+
 threading.Thread(target=loop).start()
 
 bot.infinity_polling()
